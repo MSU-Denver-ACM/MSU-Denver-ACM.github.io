9698b75b06e926917b6a90be1c6022b552cfea41
MSUDenverACM <beatys@msudenver.edu>
2019-06-27 12:28:36 -0600

    2019-06-27:12:28:36

--- beaty/script.sh
+++ beaty/script.sh
@@ -3,31 +3,17 @@
 filenames=( \
    "git_team1" \
    "git_team2" \
-   "git_team3" \
-   "git_team4" \
-   "git_team5" \
-   "git_team6" \
    "trello_team1" \
    "trello_team2" \
-   "trello_team3" \
-   "trello_team4" \
-   "trello_team5" \
-   "trello_team6" \
 )
 
 urls=( \
-"https://github.com/AllisonDLucca/3250-spring-2019-team-1/graphs/contributors" \
-"https://github.com/rhill53/CS3250SpringH2KTB/graphs/contributors" \
-"https://github.com/git-confused-team3/GitConfused/graphs/contributors" \
-"https://github.com/msudenvercs/3250-spring-2019-team-4/graphs/contributors" \
-"https://github.com/johnhshapiro/trelloship" \
-"https://github.com/rykonermana/3250-spring-2019-team-6/graphs/contributors" \
-    "https://trello.com/b/45CG5wWY" \
-    "https://trello.com/b/ZqjlOib5" \
-    "https://trello.com/b/rds2jt1w" \
-    "https://trello.com/b/K4Ub1cnv" \
-    "https://trello.com/b/CRCJVEAG" \
-    "https://trello.com/b/Fg2LIJyP" \
+    "https://github.com/GitNickProgramming/Dork/graphs/contributors" \
+    "https://github.com/team-c3250-noname/Dork/graphs/contributors" \
+    "https://github.com/cs-3250/dork/graphs/contributors" \
+    "https://trello.com/b/ADkrbwOr/" \
+    "https://trello.com/b/UH1ZblkH/" \
+    "https://trello.com/b/JcpTsXlj/" \
 )
 
 length=$((${#filenames[@]} - 1))
