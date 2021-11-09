7f76b93fe43b3003dab306387c75fe6470a0744b
drb80 <beatys@msudenver.edu>
2021-11-09 15:50:52 -0700

    2021-11-09:15:50:52

--- beaty/script.sh
+++ beaty/script.sh
@@ -1,45 +1,16 @@
 #! /bin/bash
 
-filenames=( \
-   "team1_sdlc" \
-   "team1_repo" \
-   "team2_sdlc" \
-   "team2_repo" \
-   "team3_sdlc" \
-   "team4_sdlc" \
-   "team4_repo" \
-   "team5_sdlc" \
-   "team6_sdlc" \
-   "team6_repo" \
-)
-
-#   "Cybersecurity" \
-#   "Monthly" \
-
 urls=( \
-    "https://trello.com/b/r79PxdZn/" \
-    "https://github.com/Mirroraculous/Mirroraculous/graphs/contributors" \
-    "https://trello.com/b/LFmDKfDA/" \
-    "https://github.com/team-discovery-channel/compose/graphs/contributors" \
-    "https://undecideds.atlassian.net/projects/ASA/issues/?filter=allissues&=" \
-    "https://github.com/cdvandyke/CPAP-extraction/issues" \
-    "https://github.com/cdvandyke/CPAP-extraction/graphs/contributors" \
-    "https://trello.com/b/Y64HlPKo/" \
-    "https://trello.com/b/wylqbIxE/" \
-    "https://github.com/sheet-t/HPotter/graphs/contributors" \
-    "https://msu-denver-acm.github.io/SIGS/Cybersecurity/" \
-    "https://msu-denver-acm.github.io/SIGS/Monthly_Event/" \
+    "https://github.com/Dandle-App/Dandle-Backend/graphs/contributors" \
+    "https://github.com/Dandle-App/Dandle-Frontend/graphs/contributors" \
+    "https://github.com/MSU-CS4360-Nuclear/nuclear/graphs/contributors" \
+    "https://github.com/freyakgray/TwinPrimes/graphs/contributors" \
 )
 
-length=$((${#filenames[@]} - 1))
+length=$((${#urls[@]} - 1))
 for i in $(eval echo "{0..$length}")
 do
-    filename=${filenames[$i]}
-    # echo ${urls[$i]}
-    rm -f ${filename}.png
-    pageres \
-        --filename=${filenames[$i]} \
-        --scale='.5' \
-        ${urls[$i]}
-    echo "https://raw.githubusercontent.com/MSU-Denver-ACM/MSU-Denver-ACM.github.io/beaty_web_images/beaty/${filename}.png"
+    rm -f ${i}.png
+    pageres --filename=${i} --scale='.5' ${urls[${i}]}
+    echo "https://raw.githubusercontent.com/MSU-Denver-ACM/MSU-Denver-ACM.github.io/beaty_web_images/beaty/${i}.png"
 done
